8e4a67a1caf9adadc9e02ddb9a387785be864ab6
TerrariumCode <12590206+TerrariumCode@users.noreply.github.com>
2025-10-08 17:08:27 +0100

    feat(nvim): turn on fuzzy finding

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -52,7 +52,7 @@ return {
                     },
                 },
                 fzf = {
-                    fuzzy = false,                  -- false will only do exact matching
+                    fuzzy = true,                   -- false will only do exact matching
                     override_generic_sorter = true, -- override the generic sorter
                     override_file_sorter = true,    -- override the file sorter
                     case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
@@ -90,8 +90,8 @@ return {
     },
     {
         "LinArcX/telescope-env.nvim",
-        config = function ()
-           require("telescope").load_extension("env")
+        config = function()
+            require("telescope").load_extension("env")
         end
     },
 }
