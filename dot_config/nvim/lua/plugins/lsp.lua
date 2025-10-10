6bb2f78cefec477eae3d385bfefb9d3e6b22db11
TerrariumCode <12590206+TerrariumCode@users.noreply.github.com>
2025-10-10 14:58:58 +0100

    chore: rm tailwind-tools

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -55,19 +55,6 @@ return {
         end,
     },
 
-    -- tailwind
-    {
-        "luckasRanarison/tailwind-tools.nvim",
-        name = "tailwind-tools",
-        build = ":UpdateRemotePlugins",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-telescope/telescope.nvim",
-            "neovim/nvim-lspconfig",
-        },
-        opts = {}
-    },
-
     -- typescript
     {
         "pmizio/typescript-tools.nvim",
